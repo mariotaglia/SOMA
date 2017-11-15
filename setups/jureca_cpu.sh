735efbec1c133388c29646f187fa50fed7fef31b
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2017-11-15 14:08:46 +0100

    adding a setup file to use the bathc partition of JURECA

---
+++ setups/jureca_cpu.sh
@@ -0,0 +1,8 @@
+module purge
+module load GCC
+module load ParaStationMPI
+module load CMake
+module load HDF5
+module load SciPy-Stack/2017b-Python-2.7.14
+module load h5py/2.7.1-Python-2.7.14
+export CC=gcc
