f0e9a84bfe92467470eaa512d6146fbbc0b0d634
Niklas Blagojevic <niklas@clonespy.de>
2017-11-22 21:16:25 +0000

    Revert "Revert "Merge branch 'Kuttelpumpe/SOMA-umbrella_hamiltonian' into 'umbrella_hamiltonian'""
    This reverts commit b8f8b1337554433a076a179f67416291f6603689

--- c_src/ana.c
+++ c_src/ana.c
@@ -354,6 +354,7 @@ void calc_bonded_energy(const struct Phase*const p, soma_scalar_t*const bonded_e
 			switch (bond_type) {
 			case HARMONICVARIABLESCALE:
 			    scale = p->harmonic_normb_variable_scale;
+			    /* intentionally falls through */
 			case HARMONIC:
 			    energy = p->harmonic_normb * r2 *scale;
 			    break;
@@ -638,12 +639,12 @@ int analytics(struct Phase *const p)
 	written = true;
     	}
 
-    //string_field
-        if (p->ana_info.delta_mc_string_field != 0 && p->time % p->ana_info.delta_mc_string_field == 0){
+    //umbrella_field
+        if (p->ana_info.delta_mc_umbrella_field != 0 && p->time % p->ana_info.delta_mc_umbrella_field == 0){
           if (p->info_MPI.sim_size == 1){
 #pragma acc update self(p->fields_unified[0:p->n_cells*p->n_types])
               }
-            extent_density_field(p,p->string_field,"/string_field",H5T_SOMA_NATIVE_SCALAR );
+            extent_density_field(p,p->umbrella_field,"/umbrella_field",H5T_SOMA_NATIVE_SCALAR );
             written = true;
             }
 
