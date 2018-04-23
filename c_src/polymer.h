822092a12c7de37c93291ff20f903b804b824025
Gaoyuan Wang <gaoyuan.wang@stud.uni-goettingen.de>
2018-04-23 11:29:57 +0000

    changes in rng_state_update

--- c_src/polymer.h
+++ c_src/polymer.h
@@ -126,8 +126,9 @@ int deserialize_polymer(const struct Phase*const p, Polymer*const poly,unsigned 
 //!
 //! \param p System
 //! \param poly Polymer to update
+//! \param rng_update_flag The flag deciding whether rng_state will be updated
 //! \return Errorcode
-int update_self_polymer(const struct Phase*const p,Polymer*const poly);
+int update_self_polymer(const struct Phase*const p,Polymer*const poly,const int rng_update_flag);
 
 
 #endif//POLYMER_H
