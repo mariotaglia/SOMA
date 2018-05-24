30d7c86283cb162fbc8b4336a0d3f48f0f0b8084
gaoyuan wang <gaoyuan.wang@stud.uni-goettingen.de>
2018-05-24 12:05:13 +0200

    put long chaing threshold to cmdline

--- c_src/phase.c
+++ c_src/phase.c
@@ -499,14 +499,14 @@ int mc_set_init(Phase * const p){
   Polymer *const first_poly = &p->polymers[0];
   const unsigned int poly_type = first_poly->type;
   uint32_t length_poly_start = p->poly_arch[p->poly_type_offset[poly_type]];
-  if(length_poly_start>p->num_all_beads/50)
+  if(length_poly_start>p->num_all_beads/p->args.long_chain_threshold_arg)
     num_long_chain++;
   
   for (uint64_t poly_i = 1; poly_i <p->n_polymers; poly_i++){	  
     Polymer *const this_poly = &p->polymers[poly_i];
     const unsigned int poly_type = this_poly->type;
     uint32_t length_poly_i = p->poly_arch[p->poly_type_offset[poly_type]];
-    if(length_poly_i>p->num_all_beads/50)
+    if(length_poly_i>p->num_all_beads/p->args.long_chain_threshold_arg)
       num_long_chain++;
 
     int i=poly_i-1;    
