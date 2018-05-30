361e6655131c185cc70e8cdc280bdfda7b11d7d9
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2018-05-30 09:51:07 +0200

    intel toolchain on JURECA for CPU

--- setups/jureca_cpu.sh
+++ setups/jureca_cpu.sh
@@ -1,8 +1,7 @@
 module purge
-module load GCC
-module load ParaStationMPI
+module load Intel
+module load IntelMPI
 module load CMake
 module load HDF5
-module load SciPy-Stack/2017b-Python-2.7.14
-module load h5py/2.7.1-Python-2.7.14
-export CC=gcc
+module load h5py
+export CC=icc
