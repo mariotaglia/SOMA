dbeb9ae8fc6597b9f6887c02102cfdc71db244b9
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2019-02-15 17:59:01 +0100

    use parallel hdf5 again for juwels

--- setups/juwels_cpu.sh
+++ setups/juwels_cpu.sh
@@ -1,7 +1,8 @@
 module purge
 module load Intel
 module load IntelMPI
-module load h5py/2.8.0-Python-3.6.6-serial
+module load HDF5
+#module load h5py/2.8.0-Python-3.6.6-serial
 module load CMake
 
 export CC=icc
