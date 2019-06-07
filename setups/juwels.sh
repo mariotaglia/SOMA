f11e497a3f755433f8c16ae026fd91a6c786da9c
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2019-06-07 18:47:26 +0200

    juwels fix no longer needed

--- setups/juwels.sh
+++ setups/juwels.sh
@@ -8,6 +8,6 @@ module load MVAPICH2
 module load CMake
 module load HDF5
 
-export SOMA_C_FLAGS="-Mnollvm -L$EBROOTNUMACTL/lib/ -L$EBROOTGCCCORE/lib64/ -L$EBROOTHDF5/lib/ -L$EBROOTMVAPICH2/lib64/ -L$EBROOTSZIP/lib/ -L$EBROOTCUDA/lib64/"
+#export SOMA_C_FLAGS="-Mnollvm -L$EBROOTNUMACTL/lib/ -L$EBROOTGCCCORE/lib64/ -L$EBROOTHDF5/lib/ -L$EBROOTMVAPICH2/lib64/ -L$EBROOTSZIP/lib/ -L$EBROOTCUDA/lib64/"
 
 export CC=pgcc
