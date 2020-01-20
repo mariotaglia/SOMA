2a3b0cc93bba77142a56ad713deb9eafc889779f
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2020-01-20 16:14:40 +0100

    hot fix MPI module

--- setups/juwels.sh
+++ setups/juwels.sh
@@ -4,10 +4,12 @@ module purge
 
 module load CUDA
 module load PGI
-module load MVAPICH2
+module load MVAPICH2/2.3.2-GDR
 module load CMake
 module load HDF5
 
 #export SOMA_C_FLAGS="-Mnollvm -L$EBROOTNUMACTL/lib/ -L$EBROOTGCCCORE/lib64/ -L$EBROOTHDF5/lib/ -L$EBROOTMVAPICH2/lib64/ -L$EBROOTSZIP/lib/ -L$EBROOTCUDA/lib64/"
+
+export export LD_PRELOAD=$MV2_PATH/lib64/libmpi.so
 
 export CC=pgcc
