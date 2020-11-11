cbc4d401045f37e4593ed55e910c5d56e12f2e9f
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2020-11-11 17:14:01 +0100

    explictly make CUDA devices accessible

--- setups/juwels.sh
+++ setups/juwels.sh
@@ -9,5 +9,5 @@ module load mpi-settings/CUDA
 
 #export SOMA_C_FLAGS="-Mnollvm -L$EBROOTNUMACTL/lib/ -L$EBROOTGCCCORE/lib64/ -L$EBROOTHDF5/lib/ -L$EBROOTMVAPICH2/lib64/ -L$EBROOTSZIP/lib/ -L$EBROOTCUDA/lib64/"
 
-
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export CC=nvc
