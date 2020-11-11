b87195c9e906e0f999f8442ed1e3e6c828a5585c
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2020-11-11 15:52:02 +0100

    new configuration files juwels

--- setups/juwels.sh
+++ setups/juwels.sh
@@ -1,13 +1,13 @@
 module purge
 
 module load CUDA
-module load PGI
-module load MVAPICH2
+module load NVHPC
+module load ParaStationMPI
 module load CMake
 module load HDF5
+module load mpi-settings/CUDA
 
 #export SOMA_C_FLAGS="-Mnollvm -L$EBROOTNUMACTL/lib/ -L$EBROOTGCCCORE/lib64/ -L$EBROOTHDF5/lib/ -L$EBROOTMVAPICH2/lib64/ -L$EBROOTSZIP/lib/ -L$EBROOTCUDA/lib64/"
 
-export MV2_USE_GPUDIRECT_GDRCOPY=0
 
-export CC=pgcc
+export CC=nvc
