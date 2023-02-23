1bf10ff04cd9bb03d1990cdc39a1ccb1156c7577
Mario Tagliazucchi <mtaglia@Lursa.theorie.physik.uni-goettingen.de>
2023-02-23 11:25:36 +0100

    Working version with two solvers

--- c_src/donnan.c
+++ c_src/donnan.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include "mpiroutines.h"
 #include "soma_config.h"
+#include "float.h"
 
 int call_donnan(const struct Phase *const p);
 
@@ -10,18 +11,59 @@ int call_donnan(const struct Phase *const p);
 int call_donnan(const struct Phase *const p)
 {
 unsigned int i, type;
-soma_scalar_t  rhoQ[p->n_cells_local]; // total charge density
+soma_scalar_t  rhoQ[p->n_cells_local]; // total number of charges
+soma_scalar_t  Qpos, Qneg, Qposnew, Qnegnew; 
+soma_scalar_t iterror = DBL_MAX;
 
-for (i = 0 ; i < p->n_cells_local ; i++) {
-        rhoQ[i] = 0.0 ; 
+const soma_scalar_t  deltax = p->Lx/((soma_scalar_t) p->nx);
+const soma_scalar_t  deltay = p->Ly/((soma_scalar_t) p->ny);
+const soma_scalar_t  deltaz = p->Lz/((soma_scalar_t) p->nz);
+const soma_scalar_t  vcell = deltax*deltay*deltaz;
+const soma_scalar_t  vall = p->Lx*p->Ly*p->Lz ;
+ 
+const soma_scalar_t maxiterror = 1e-6*vall ; // maximum iteration error for Qpos and Qneg
+
+Qneg = vall;
+Qpos = vall;
+
+while (iterror > maxiterror) {
+    Qnegnew = 0.0;
+    Qposnew = 0.0; 
+
+    for (i = 0 ; i < p->n_cells_local ; i++) {
+        rhoQ[i] = 0.0; 
         for (type = 0 ; type < p->n_types; type++) {
                    rhoQ[i] += p->fields_unified[i+p->n_cells_local*type]*p->charges[type];
         } 
+        rhoQ[i] = rhoQ[i] / vcell ; // units of charge per Re^3
 
-   	p->electric_field[i] = rhoQ[i] + sqrt(rhoQ[i]*rhoQ[i] + 4.*p->Nions*p->Nions) ;
-	p->electric_field[i] = p->electric_field[i] / (2.0*p->Nions) ;
+
+     if (p->Nnegions > p->Nposions) {    	
+	p->electric_field[i] = rhoQ[i];
+	p->electric_field[i] += sqrt(rhoQ[i]*rhoQ[i] + 4.*p->Nposions*p->Nnegions/Qpos/Qneg) ;
+	p->electric_field[i] = p->electric_field[i] / (2.0*p->Nnegions/Qneg) ;
 	p->electric_field[i] = log(p->electric_field[i]);
-}
+     }
+     else {
+	p->electric_field[i] = -rhoQ[i];
+	p->electric_field[i] += sqrt(rhoQ[i]*rhoQ[i] + 4.*p->Nposions*p->Nnegions/Qpos/Qneg) ;
+	p->electric_field[i] = p->electric_field[i] / (2.0*p->Nposions/Qpos) ;
+	p->electric_field[i] = -log(p->electric_field[i]);
+     }
 
+        Qposnew += exp(-p->electric_field[i]);
+        Qnegnew += exp(p->electric_field[i]);
+    }
+
+        Qposnew = Qposnew*vcell;
+        Qnegnew = Qnegnew*vcell;
+
+        iterror = fabs(Qpos-Qposnew) + fabs(Qneg-Qnegnew);
+	Qpos = Qposnew;
+	Qneg = Qnegnew;
+
+//        printf("Qpos, Qneg, error, %.3e %.3e %.3e \n", Qpos, Qneg, iterror);
+}
+	
   return(0);
 }
--- c_src/mesh.c
+++ c_src/mesh.c
@@ -316,8 +316,9 @@ void update_omega_fields(const struct Phase *const p)
     //
     if (p->efieldsolver == 0) 
     	call_kinsol(p);
-    else if (p->efieldsolver == 1)
+    else if (p->efieldsolver == 1) {
     	call_donnan(p);
+    }
 
     switch (p->hamiltonian)
         {
@@ -519,22 +520,3 @@ void calc_ions(struct Phase *const p)
 
 }
 
-
-int call_donnan(const struct Phase *const p)
-{
-unsigned int i, type;
-soma_scalar_t  rhoQ[p->n_cells_local]; // total charge density
-
-for (i = 0 ; i < p->n_cells_local ; i++) {
-        rhoQ[i] = 0.0 ; 
-        for (type = 0 ; type < p->n_types; type++) {
-                   rhoQ[i] += p->fields_unified[i+p->n_cells_local*type]*p->charges[type];
-        } 
-
-   	p->electric_field[i] = rhoQ[i] + sqrt(rhoQ[i]*rhoQ[i] + 4.*p->Nions*p->Nions) ;
-	p->electric_field[i] = p->electric_field[i] / (2.0*p->Nions) ;
-	p->electric_field[i] = log(p->electric_field[i]);
-}
-
-  return(0);
-}
