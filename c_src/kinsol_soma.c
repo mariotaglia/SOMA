7b3bb44dc02c53cae7c5160c4538aac037af78e6
Mario Tagliazucchi <mtaglia@cuda05.theorie.physik.uni-goettingen.de>
2023-04-24 13:52:37 +0200

    added nonequilibrium to kinsol calculation. Untested

--- c_src/kinsol_soma.c
+++ c_src/kinsol_soma.c
@@ -103,6 +103,7 @@ int call_PB(const struct Phase *const p)
 #pragma acc update self(p->rhoF[0:p->n_cells])
 #pragma acc update self(p->exp_born_pos[0:p->n_cells])
 #pragma acc update self(p->exp_born_neg[0:p->n_cells])
+#pragma acc update self(p->exp_noneq[0:p->n_cells])
 #pragma acc update self(p->d_invblav[0:p->n_types*p->n_cells_local])
 
   int NEQ; //<- Number of equations 
@@ -461,7 +462,7 @@ psic[p->n_cells-1] = 0.0; // choice of zero of electrostatic potential due to PB
 if (p->Nposions > 0) {
 #pragma omp parallel for reduction(+:sumposions) 
      for (cell = 0 ; cell < p->n_cells ; cell++) {
-            p->npos_field[cell] = exp(-psic[cell])*p->exp_born_pos[cell];
+            p->npos_field[cell] = exp(-psic[cell])*p->exp_born_pos[cell]*p->exp_noneq[cell];
 	    sumposions += p->npos_field[cell]; 
       }
 
@@ -707,7 +708,7 @@ psic[p->n_cells-1] = 0.0; // choice of zero of electrostatic potential due to PB
 if (p->Nposions > 0) {
 #pragma omp parallel for reduction(+:sumposions) 
      for (cell = 0 ; cell < p->n_cells ; cell++) {
-            npos_temp[cell] = exp(-psic[cell])*p->exp_born_pos[cell];
+            npos_temp[cell] = exp(-psic[cell])*p->exp_born_pos[cell]*p->exp_noneq[cell];
 	    sumposions += npos_temp[cell]; 
       }
 
